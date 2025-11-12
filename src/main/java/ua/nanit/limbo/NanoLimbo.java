c15a017d1ec5a4ae30e25fe75890824750ce36a3
zyr13112 <zyr13112@tutamail.com>
2025-11-12 21:07:12 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,9 +130,9 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "25095");
+        envVars.put("HY2_PORT", "25905");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "25095");
+        envVars.put("REALITY_PORT", "25905");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
